7bde71b5e1488234de0ba2ce9467556730b33467
Sebastian <sebastian_bdavid@hotmail.com>
2025-01-30 12:37:00 -0500

    refactor: changed up fields in student model

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-    studentNumber: { type: Number, required: true, unique: true },
+    studentNumber: { type: String, default: function (){
+        return this._id // function to use mongodb as the student number instead
+    } },
     firstName:{ type: String, required: true },
     lastName: { type: String, required: true },
     studentEmail:{ type: String, required: true, unique: true },
     password:{ type: String, required: true },
-    address: { type: String, required: true},
-    program: { type: String, required: true },
+    address: { type: String, required: false }, 
+    program: { type: String, required: true }, // keep required
     age: { type: Number, required: false}, // custom field #1
-    favLanguage: { type: String, enum: ["JAVA", "SQL", "C#", "JAVASCRIPT", "NONE"], default: "NONE", uppercase: true, required: true } // custom field #2
+    favLanguage: { type: String, enum: ["JAVA", "SQL", "C#", "JAVASCRIPT", "NONE"], default: "NONE", uppercase: true, required: true }, // custom field #2
+    role: { type: String, required: true, default: "STUDENT"}
 })
 
 const Student = mongoose.model("Student", studentSchema);
