e3b350944a214a56bd82fe38e99f2e9d93f80ea1
Sebastian <sebastian_bdavid@hotmail.com>
2025-01-30 18:12:08 -0500

    feat: added ability to add many courses

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -2,16 +2,27 @@ const Course = require("../models/course.js")
 
 const createCourse = async ( req, res) => {
     try {
-        const course = new Course(req.body);
-        await course.save();
-        res.status(200).json({
+        if (Array.isArray(req.body)){
+            const courses = await Course.insertMany(req.body);
+            res.status(200).json({
+                message: "Created courses successfully",
+                courses: courses
+            });
+        }
+        else {
+            const course = new Course(req.body);
+            await course.save();
+            res.status(200).json({
             message: "Created course successfully \n" + course,
-        })
+            })
+        }   
+        
     }
     catch(error){
         res.status(400).json({message: error.message})
     }
 }
+
 module.exports ={
     createCourse,
 }
