d0424874e96b3f19c39d074e8e52247be697df30
Sebastian <sebastian_bdavid@hotmail.com>
2025-01-30 17:18:51 -0500

    feat: added login, and token generation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,38 @@ const registerStudent = async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 }
+const loginStudent = async (req, res) => {
+    try{
+        const { studentEmail, password } = req.body;
+        if (!studentEmail || !password) {
+            throw new Error ("Please fill all fields, thanks!")
+        }
+        // look for student
+        const student = await Student.findOne({ studentEmail});
+        if (!student || student.password !== password){
+            throw new Error ("Incorrect email or password")
+        }
+        
+        // generate token
+        const token = jwt.sign(
+            { _id: student._id },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "1h",
+            }
+          );
+      
+          res.status(200).json({ 
+            message: "Log in successful",
+            token 
+        });
 
+    }
+    catch (error){
+        res.status(400).json({message: error.message})
+    }
+}
 module.exports = {
     registerStudent,
+    loginStudent,
 }
--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { registerStudent } = require("../controllers/authController.js")
+const { registerStudent, loginStudent } = require("../controllers/authController.js")
 
 router.post("/register", registerStudent);
+router.post("/login", loginStudent);
 
 module.exports = router;
