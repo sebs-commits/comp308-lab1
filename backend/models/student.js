2f0732627542e699c21d594ffc8e41819fd833a0
Sebastian <sebastian_bdavid@hotmail.com>
2025-01-29 22:36:15 -0500

    feat: added more fields to student model

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-    studentName:{ type: String, required: true },
+    studentNumber: { type: Number, required: true, unique: true },
+    firstName:{ type: String, required: true },
+    lastName: { type: String, required: true },
     studentEmail:{ type: String, required: true, unique: true },
     password:{ type: String, required: true },
+    address: { type: String, required: true},
+    program: { type: String, required: true },
+    age: { type: Number, required: false}, // custom field #1
+    favLanguage: { type: String, enum: ["JAVA", "SQL", "C#", "JAVASCRIPT", "NONE"], default: "NONE", uppercase: true, required: true } // custom field #2
 })
 
 const Student = mongoose.model("Student", studentSchema);
