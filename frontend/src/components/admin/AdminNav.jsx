84c468a7c48af639b4efebad0810f16f1cb622ad
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-04 16:11:23 -0500

    feat: created adminnav

---
+++ frontend/src/components/admin/AdminNav.jsx
@@ -0,0 +1,58 @@
+import { authService } from '../../services/auth.service.js';
+import { useNavigate } from 'react-router-dom';
+const AdminNav = () => {
+
+    const navigate = useNavigate();
+    const handleLogout = async (e) =>{
+        e.preventDefault();
+        try{
+            const response = await authService.logout();
+            navigate("/login")
+            console.log("logged out successfully")
+
+        } catch (error) {
+            console.error('Failed to logout:', error.response?.data?.message || 'An error occurred');
+        }
+    }
+    return (
+    <div className="navbar bg-base-100">
+        <div className="navbar-start">
+            <div className="dropdown">
+            <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+            </div>
+            <ul
+                tabIndex="0"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li><a>My Courses</a></li>
+                <li><a>Profile</a></li>
+            </ul>
+            </div>
+            <a className="btn btn-ghost text-xl">Admin</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+            <li><a>Add Student</a></li>
+            <li><a>List All Students</a></li>
+            <li><a>List All Courses</a></li>
+            </ul>
+        </div>
+        <div className="navbar-end">
+            <a className="btn" onClick={handleLogout}>Logout</a>
+        </div>
+    </div>
+    );
+};
+
+export default AdminNav;
