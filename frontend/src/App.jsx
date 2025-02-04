7c110d1781dcc87539d8bdcfc6d022e189840329
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-04 11:15:36 -0500

    feat: added route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from "./components/auth/LoginForm.jsx"
 import StudentHome from "./components/student/StudentHome.jsx";
 
 function App() {
 
+  const isAuthenticated = localStorage.getItem('token');
+
   return (
     <>
       <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login"/>}/>
         <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/student" element={<StudentHome/>}/>
+        <Route path="/student" element={isAuthenticated ? <StudentHome/> : <Navigate to="/login"/>} />
 
       </Routes>
       </BrowserRouter>
