e677f53cb929615a04ed923b33e23fb2f0da3b3f
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-04 00:48:45 -0500

    style: refactored student nav

--- frontend/src/components/student/StudentHome.jsx
+++ frontend/src/components/student/StudentHome.jsx
@@ -7,7 +7,9 @@ const StudentHome = () => {
         <div className="min-h-screen bg-gray-100">
             <StudentNav />
             <div className="container mx-auto p-4">
-                
+                <h1 className="text-2xl font-semibold text-gray-900">Student Home Dashboard</h1>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/student/StudentNav.jsx
+++ frontend/src/components/student/StudentNav.jsx
@@ -1,16 +1,41 @@
 const StudentNav = () => {
     return (
-        <div className="navbar bg-base-100 shadow-md">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Student Dashboard</a>
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Profile</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
+            <ul
+                tabindex="0"
+                class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li><a>My Courses</a></li>
+                <li><a>Profile</a></li>
+            </ul>
             </div>
+            <a class="btn btn-ghost text-xl">Student</a>
         </div>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+            <li><a>My Courses</a></li>
+            <li><a>Profile</a></li>
+            </ul>
+        </div>
+        <div class="navbar-end">
+            <a class="btn">Logout</a>
+        </div>
+    </div>
     );
 };
 
