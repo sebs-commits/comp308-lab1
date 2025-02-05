320bde39bf7964f12621e051fcfbafbf9e6bad57
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-05 18:50:31 -0500

    feat: created footer - updated title - removed vite icon - added footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginForm from "./components/auth/LoginForm.jsx"
+import LoginForm from "./components/auth/LoginForm.jsx";
 import StudentHome from "./components/student/StudentHome.jsx";
-import AdminHome from "./components/admin/AdminHome.jsx"
+import AdminHome from "./components/admin/AdminHome.jsx";
+import Footer from "./components/common/Footer.jsx";
 
 function App() {
 
@@ -16,9 +17,9 @@ function App() {
         <Route path="/" element={<Navigate to="/login"/>}/>
         <Route path="/login" element={<LoginForm/>}/>
         <Route path="/student" element={isAuthenticated ? <StudentHome/> : <Navigate to="/login"/>} />
-
       </Routes>
       </BrowserRouter>
+      <Footer/>
     </>
   )
 }
---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Created by Sebastian David</p>
+                <p>COMP-308 | Assignment 1</p>
+            </aside>
+        </footer>
+    )
+}
+export default Footer;
